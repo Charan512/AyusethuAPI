74e214c63d89883c406dfbfd94c455a79189c5bc
sri ram charan <nvsk72@gmail.com>
2026-04-08 11:30:02 +0530

    Increase backend payload limit for Audio

--- src/server.js
+++ src/server.js
@@ -29,8 +29,8 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const io = new Server(httpServer, {
   cors: corsOptions
